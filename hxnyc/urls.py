a3d927c847ebfd381442d08a2b9138fd36a06860
synergit <syner.wang@gmail.com>
2020-11-10 10:06:50 -0500

    try to fix the static file collection issue

--- hxnyc/urls.py
+++ hxnyc/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r"^", include("course.urls")),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
