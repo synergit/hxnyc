d0bc6cca9690dec283d980760f47bff11a6dd0f6
synergit <syner.wang@gmail.com>
2020-11-10 23:38:50 -0500

    WIP: needs to make regex more clear

--- course/urls.py
+++ course/urls.py
@@ -6,6 +6,6 @@ from course import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    url(r"^", views.index, name="index"),
+    url(r"^index", views.index, name="index"),
     url(r'^course/(?P<course_id>\d+)/$', views.detail, name='detail'),
 ]
