b1a4b4f757f82df844b50691672e65b9a15f11c0
synergit <syner.wang@gmail.com>
2020-11-12 21:56:22 -0500

    removed the STATIC_ROOT which is only for development

--- hxnyc/urls.py
+++ hxnyc/urls.py
@@ -22,4 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r"^", include("course.urls")),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  ]
+# Below is only for development
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
