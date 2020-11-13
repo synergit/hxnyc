e4659912fbe6649e0cc4f6d0b4e86b7d6865aee1
synergit <syner.wang@gmail.com>
2020-11-12 21:55:42 -0500

    added STATIC_ROOT

--- settings/base.py
+++ settings/base.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
 try:
     from .secret_key import *
     SECRET_KEY = SECRET_KEY
@@ -125,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
 
 
 # for production env. 
