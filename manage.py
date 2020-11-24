44a56da5576022f6f148693e58e120021bdff602
synergit <syner.wang@gmail.com>
2020-11-23 23:20:32 -0500

    removed unused django-prometheus integration

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-from opentelemetry.instrumentation.django import DjangoInstrumentor
+# from opentelemetry.instrumentation.django import DjangoInstrumentor
 
 def main():
     """Run administrative tasks."""
